e128747556190a996cd7668df0e5bb3fc67b1803
Marcos Cárceles <mcarceles@tuenti.com>
2020-12-05 01:10:35 +0100

    Part 2

--- 4/passport-processing.py
+++ 4/passport-processing.py
@@ -1,7 +1,10 @@
 import sys
 import re
+import logging
 
 from functools import reduce
+
+# logging.basicConfig(level=logging.DEBUG)
 
 class Passport:
     """Passport Representation"""
@@ -14,9 +17,57 @@ class Passport:
     pid = None
     cid = None
 
+    @staticmethod
+    def __numberInRange(string, rangeTup):
+        start, end = rangeTup
+        return string and string.isnumeric() and int(string) in range(start, end+1)
+
     def valid(self):
-        return self.byr and self.iyr and self.hgt and self.hcl and self.ecl and self.pid
+        valid = bool(self.validByr() and self.validYir() and self.validEyr() and self.validHgt() and self.validHcl() and self.validEcl() and self.validPid())
+        logging.info("Passport %s is %s" % (vars(self), valid))
+        return valid
+
+    def validByr(self):
+        valid = self.__numberInRange(self.byr, (1920, 2002))
+        logging.debug("\tvalidByr: %s" % valid)
+        return valid
+
+    def validYir(self):
+        valid = self.__numberInRange(self.iyr, (2010, 2020))
+        logging.debug("\tvalidYir: %s" % valid)
+        return valid
+
+    def validEyr(self):
+        valid = self.__numberInRange(self.eyr, (2020, 2030))
+        logging.debug("\tvalidEyr: %s" % valid)
+        return valid
+
+    def validHgt(self):
+        valid = False
+        if self.hgt:
+            numeric = re.match(r"\d+", self.hgt)
+            if re.match(r"^\d+cm$", self.hgt):
+                valid = self.__numberInRange(numeric[0], (150, 193))            
+            elif re.match(r"^\d+in$", self.hgt):
+                valid = self.__numberInRange(numeric[0], (59, 76))
+        logging.debug("\tvalidHgt: %s" % valid)
+        return valid
+
+    def validHcl(self):
+        valid = bool(self.hcl and re.match(r"^\#[0-9a-f]{6}$", self.hcl))
+        logging.debug("\tvalidHcl: %s" % valid)
+        return valid
     
+    def validEcl(self):
+        valid = self.ecl in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth')
+        logging.debug("\tvalidEcl: %s" % valid)
+        return valid
+    
+    def validPid(self):
+        valid = bool(self.pid and re.match(r"^\d{9}$", self.pid))
+        logging.debug("\tvalidPid: %s" % valid)
+        return valid
+
     def empty(self):
         return not self.byr and not self.iyr and not self.hgt and not self.hcl and not self.ecl and not self.pid and not self.cid
 
